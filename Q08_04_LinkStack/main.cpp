1bb0df97ecd59f971373f9a85cd34676b9b2af74
liubincodelife <liubinmcu@sina.com>
2019-04-07 00:21:33 +0800

    implement the broser function back and forwad simulated

--- Q08_04_LinkStack/main.cpp
+++ Q08_04_LinkStack/main.cpp
@@ -1,26 +1,79 @@
 #include <iostream>
 #include "LinkStack.hpp"
+#include <string>
 
 using namespace std;
 
+LinkStack<string> backStack;
+LinkStack<string> forwardStack;
+string currentPage;
+
+bool visitNewPage(string pageUri)
+{
+    while (forwardStack.getSize() > 0)
+    {
+        string tempPage = forwardStack.top();
+        forwardStack.pop();
+        cout << "clear pages in forwardStack :" << tempPage << endl;
+    }
+    currentPage = pageUri;
+    backStack.push(pageUri);
+    cout << "current page is :" << currentPage << endl;
+
+    return true;
+}
+
+bool gotoBackPage()
+{
+    currentPage = backStack.top();
+    backStack.pop();
+    forwardStack.push(currentPage);
+    currentPage = backStack.top();
+    cout << "current page is :" << currentPage << endl;
+    return true;
+}
+
+bool gotoForwardPage()
+{
+    currentPage = forwardStack.top();
+    forwardStack.pop();
+    backStack.push(currentPage);
+    cout << "current page is :" << currentPage << endl;
+
+    return true;
+}
+
 int main(int argc, char** argv)
 {
-    LinkStack<int> linkStack;
-    cout << "push data into stack:" << endl;
-    for (int i = 1; i < 10; i++)
-    {
-        linkStack.push(i);
-        cout << i << " ";
-    }
-    cout << endl;
-    cout << "pop data out to stack:" << endl;
-    size_t size = linkStack.getSize();
-    for (int i = 0; i < size; i++)
-    {
-        int retValue = linkStack.top();
-        linkStack.pop();
-        cout << retValue << " ";
-    }
+    string homePage = "www.google.com";
+    cout << "visit home page first..." << endl;
+    string currentPage = homePage;
+    visitNewPage(currentPage);
+    cout << "visit a new page 01..." << endl;
+    currentPage = "www.google.com/uri_001";
+    visitNewPage(currentPage);
+    cout << "visit a new page 02..." << endl;
+    currentPage = "www.google.com/uri_002";
+    visitNewPage(currentPage);
+
+    cout << "go back to previous page 01..." << endl;
+    gotoBackPage();
+
+    cout << "go forward to next page 01..." << endl;
+    gotoForwardPage();
+
+    cout << "go back to previous page 02..." << endl;
+    gotoBackPage();
+
+    cout << "go back to previous page 03..." << endl;
+    gotoBackPage();
+
+    cout << "visit a new page 03..." << endl;
+    currentPage = "www.google.com/uri_003";
+    visitNewPage(currentPage);
+
+    cout << "go back to previous page 04..." << endl;
+    gotoBackPage();
     
     getchar();
     return 0;
